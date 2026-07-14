35664e40d070b9e74ee6298fd885668f43506d3b
Keisuke Marutani <132348090+marutyan@users.noreply.github.com>
2026-07-14 16:58:09 +0900

    feat: start automatic group sorter

--- entrypoints/background.ts
+++ entrypoints/background.ts
@@ -1,8 +1,14 @@
 import { AutoGroupingController } from "../src/background/controller";
+import { TabSorter } from "../src/background/tab-sorter";
 
 export default defineBackground(() => {
   const controller = new AutoGroupingController();
-  void controller.start().catch((error: unknown) => {
+  const sorter = new TabSorter();
+
+  void (async () => {
+    await controller.start();
+    await sorter.start();
+  })().catch((error: unknown) => {
     console.error("AutoGrouping failed to start", error);
   });
 });
