e9d5f951fbb87851ecd706aeaece17b42f20530c
marutyan <marutyan310@gmail.com>
2026-07-14 17:16:25 +0900

    feat: add AutoGrouping extension icon

--- wxt.config.ts
+++ wxt.config.ts
@@ -9,8 +9,20 @@ export default defineConfig({
     version: "0.1.0",
     minimum_chrome_version: "120",
     permissions: ["storage", "tabs", "tabGroups", "contextMenus"],
+    icons: {
+      16: "icon/16.png",
+      32: "icon/32.png",
+      48: "icon/48.png",
+      128: "icon/128.png",
+    },
     action: {
       default_title: "AutoGrouping",
+      default_icon: {
+        16: "icon/16.png",
+        32: "icon/32.png",
+        48: "icon/48.png",
+        128: "icon/128.png",
+      },
     },
   },
 });
