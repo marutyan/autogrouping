a16cf2b8627a3c7f1454ba247f78a220965a90d0
Keisuke Marutani <132348090+marutyan@users.noreply.github.com>
2026-07-14 17:46:34 +0900

    refactor: remove unused JSON settings page

--- entrypoints/options/main.tsx
+++
@@ -1,7 +0,0 @@
-import { createRoot } from "react-dom/client";
-import { OptionsApp } from "./OptionsApp";
-import "./style.css";
-
-const root = document.getElementById("root");
-if (!root) throw new Error("Options root element is missing");
-createRoot(root).render(<OptionsApp />);
